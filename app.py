e9582cb27951a953b42211baa0f7c0c3b07dc6e9
Sam Murgatroyd <sammurgatroyd30@gmail.com>
2023-10-05 10:10:39 +0100

    app.py update commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
-
+from lib.recipe_repository import RecipeRepository
 
 # Connect to the database
 connection = DatabaseConnection()
@@ -16,3 +16,8 @@ artists = artist_repository.all()
 # List them out
 for artist in artists:
     print(artist)
+
+connection.seed('seeds/recipes.sql')
+recipe_repo = RecipeRepository(connection)
+result = recipe_repo.find(3)
+print(result)
